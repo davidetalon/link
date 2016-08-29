6dff29f907ab6a77e93f5fdf15478d2b6bb17503
Davide Talon <tal94@hotmail.it>
2016-08-29 23:02:26 +0200

    prova client funzionante

--- prova_client.c
+++ prova_client.c
@@ -1,3 +1,14 @@
+/**
+* @prova_client.c
+* @brief open a TCP socket to Link master
+*
+* @author Davide Talon (<talon.davide@gmail.com>)
+* @version 1.0
+* @since 1.0
+*
+* @copyright Copyright (c) 2016-2017
+* @copyright Apache License, Version 2.0
+*/
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -7,12 +18,20 @@
 
 int main (void) {
 
+/**IDEA
+* Creo un socket UDP su una porta random, invio un messaggio in broadcast e attendo la risposta del serve
+* contenente ip, porta e nome. Li elenco ed aspetto che l'utente scelga il master di destinazione.
+* Una volta scelto il master apro un socket TCP con il master per inviare i dati.
+*/
+
+
 
 /** 
 *	Link application settings
 */
 	//used port for application
-	int port = 9091;
+	int client_port = 9092;
+	int server_port = 9091;
 
 
 /** 
@@ -24,7 +43,7 @@ int main (void) {
 	memset(&server_address, 0, sizeof(server_address));
 
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(port);
+	server_address.sin_port = htons(server_port);
 	server_address.sin_addr.s_addr = inet_addr("192.168.1.54");
 
 
@@ -39,7 +58,7 @@ int main (void) {
 	memset(&client_address, 0, sizeof(client_address));
 
 	client_address.sin_family = AF_INET;
-	client_address.sin_port = htons(port);
+	client_address.sin_port = htons(client_port);
 	client_address.sin_addr.s_addr = inet_addr("192.168.1.55");
 	
 
@@ -51,21 +70,22 @@ int main (void) {
 	}
 
 	//client buffer
-	char buffer[256] = "AHAHAHAHHAHA";
+	char buffer[256];
 	int buffer_size = sizeof(buffer);
 
-	/*if (bind(client_socket, (struct sockaddr *) &client_address, sizeof(client_address)) < 0) {
-		perror("Socket not binded");
-		// exit(1);
-	} */
 
 	if (connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
       perror("Cannot connect to server");
       // exit(1);
    } else {
-   		printf("Connection enstabilised\n");
-   		send(client_socket,buffer, sizeof(buffer), 0);
-   		printf("Message sent\n");
+   		// printf("Connection enstabilised\n");
+   		// send(client_socket,buffer, sizeof(buffer), 0);
+   		// printf("Message sent\n");
+   		do {
+
+   			scanf("%256s", buffer);
+   			send(client_socket, buffer, sizeof(buffer), 0);
+   		} while (1==1);
    }
 
    	close(client_socket);
