d88f5fc670e57bdcd646b4a0011f61fa534d8345
Davide Talon <tal94@hotmail.it>
2016-09-07 01:12:11 +0200

    Implementing bynary copy on the buffer

---
+++ reading_files.c
@@ -0,0 +1,49 @@
+/**
+* @reading_files.c
+* @connect master to sleave
+*
+* @author Davide Talon (<talon.davide@gmail.com>)
+* @version 1.0
+* @since 1.0
+*
+* @copyright Copyright (c) 2016-2017
+* @copyright Apache License, Version 2.0
+*/
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define PERMS 0666
+
+int main (int argc, char *argv[]) {
+
+	if(argc == 2) {
+		printf("The argoment is: %s\n", argv[1]);
+	}
+
+	char buffer[20];
+	memset(&buffer, 0, sizeof(buffer));
+
+	FILE 	*fpi, *fpo;
+
+		fpi = fopen("Drone.tar.gz", "r");
+		// fpi = fopen("prova.txt", "r");
+		fpo = fopen("CopiaDrone.tar.gz", "w");
+
+		fseek(fpi, 0, SEEK_SET);
+		while (!feof(fpi)) {
+			fread(buffer, 20, 1, fpi);
+			// printf("%s", buffer);
+			fwrite(buffer, 20, 1, fpo);
+		}
+
+   
+		fclose(fpo);
+		fclose(fpi);
+
+	
+
+	return 0;
+
+}
