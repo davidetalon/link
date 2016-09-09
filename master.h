09c7d15527f444ca729a03c78b2d0491180926b3
Davide Talon <tal94@hotmail.it>
2016-09-09 10:43:42 +0200

    header for master

---
+++ master.h
@@ -0,0 +1,30 @@
+/**
+* @master.h
+* @header for master
+*
+* @author Davide Talon (<talon.davide@gmail.com>)
+* @version 1.0
+* @since 1.0
+*
+* @copyright Copyright (c) 2016-2017
+* @copyright Apache License, Version 2.0
+*/
+
+#ifndef MASTER_H_INCLUDED   /* Include guard */
+#define MASTER_H_INCLUDED
+
+
+//open an UDP server
+int openUdpSrv(const int udpSrvSock, const struct sockaddr_in *udpClntSockAddr, int udpClntSockAddrLen, const char *name);
+
+//open a TCP server
+int openTcpSrv(const int tcpSrvSock, struct sockaddr_in *tcpSrvSockAddr, const int tcpSrvSockAddrLen, 
+	struct sockaddr_in *tcpClntSockAddr, int tcpClntSockAddrLen, int *connectionSock);
+
+//receive file
+int receiveFile (const int *connectionSock, const char *fileName);
+
+//accept to receive file
+int acceptFile(const int *connectionSock, char **fileName, char **senderName);
+
+#endif 
